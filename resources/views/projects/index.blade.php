ac2e484a9f58e50af70d804dd05e6cca955187a7
Salma BERROUK <salmaberrouk4@gmail.com>
2025-04-21 21:36:02 +0200

    feat: display projects on the index page

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,21 @@
+<x-app-layout>
+    <h1>Mes projets</h1>
+    <a href="{{ route('projects.create') }}" class="btn btn-primary">Créer un projet</a>
+
+    @if($projects && $projects->count() > 0)
+        @foreach($projects as $project)
+            <div class="project">
+                <h3>{{ $project->name }}</h3>
+                <a href="{{ route('projects.edit', $project) }}" class="btn btn-warning">Modifier</a>
+                <form action="{{ route('projects.destroy', $project) }}" method="POST" style="display: inline;">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger" onclick="return confirm('Êtes-vous sûr de vouloir supprimer ce projet ?')">Supprimer</button>
+                </form>
+            </div>
+        @endforeach
+    @else
+        <p>Aucun projet trouvé.</p>
+    @endif
+
+</x-app-layout>

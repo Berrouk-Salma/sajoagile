60ab3296eea46c242b84a564aaa7023b2287395f
Berrouk-Salma <salmaberrouk4@gmail>
2025-04-23 14:17:41 +0100

    fix the rouls admin

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create admin user
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin',
+        ]);
+    
+
+        // Create team lead user
+        User::create([
+            'name' => 'Team Lead',
+            'email' => 'lead@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'lead',
+        ]);
+
+        // Create regular user
+        User::create([
+            'name' => 'Regular User',
+            'email' => 'user@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'user',
+        ]);
+    }
+}
